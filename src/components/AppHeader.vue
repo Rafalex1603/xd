c8397794d2a3f3e6c53c5eca5d4aadaf338dce0a
unknown <rafalexwtf@gmail.com>
2025-01-11 16:36:30 -0400

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+    <header>
+        <h1><span class="icon" arian-hidden="true">Lista de tarea</span></h1>
+        <p>Metas 2025</p>
+    </header>
+</template>
+
+<style scoped>
+    h1 {
+        font-size: 2rem;
+    }
+
+    h1 .icon {
+        font-size: 1rem;
+        
+    }
+</style>
